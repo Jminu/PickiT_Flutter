aa9618669ba8b10b1bf35928dc0f6b3bf0658378
Jin Minu <s9430939@naver.com>
2024-11-13 13:58:16 +0900

    유저 키워드목록 객체배열로 오는지 테스트 코드

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,11 @@ import './Keyword.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(); //initializeApp가 처리되는걸 대기
+
+  final User user = User("minu", "1234");
+  final userKeywordList = await user.getUserKeywords(); //유저의 키워드 목록 가져오는것을 대기
+  print(userKeywordList);
+
   runApp(const MyApp());
 }
 
@@ -16,30 +21,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // User 객체 생성과 동시에 Future도 클래스 레벨에서 한 번만 생성
-    final User user = User("minu", "1234");
-    final Future<List<Map<dynamic, dynamic>>> userKeywordsFuture =
-        user.getUserKeywords();
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Firebase'),
         ),
-        body: FutureBuilder(
-          future: userKeywordsFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              // 로딩 중일 때 로딩 표시를 보여줌
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              // 에러 발생 시 에러 메시지 출력
-              return Center(child: Text("Error: ${snapshot.error}"));
-            } else {
-              // 데이터 로드 완료 후 UI 표시
-              return const Center(child: Text('Firebase TEST'));
-            }
-          },
+        body: const Center(
+          child: Text('Firebase TEST'), // 기본 텍스트만 표시
         ),
       ),
     );
