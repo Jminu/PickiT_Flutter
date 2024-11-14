4cd9a3e7313c8bd70107b92ae18553daa72011f7
Hwnsgus <h010617@naver.com>
2024-11-14 19:00:54 +0900

    뉴스 기사창 dart파일 생성
    뉴스 기사 제목 클릭시 본문이 보이도록 하는 dart 파일 추가

---
+++ lib/models/article.dart
@@ -0,0 +1,14 @@
+
+class Article {
+  final String title;
+  final String date;
+  final String imageUrl;
+  final String content;
+
+  Article({
+    required this.title,
+    required this.date,
+    required this.imageUrl,
+    required this.content,
+  });
+}
