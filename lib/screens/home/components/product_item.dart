f3e65880fbe064cb142d2ae319181192f0fc5288
chmh <chbasis331@naver.com>
2024-11-24 18:33:42 +0900

    대략적인 화면 구성

--- lib/screens/home/components/product_item.dart
+++ lib/screens/home/components/product_item.dart
@@ -11,20 +11,20 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 135.0,
-      padding: const EdgeInsets.all(16.0),
-      child: Row(
+      height: 338.0,
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
         children: [
           ClipRRect(
             borderRadius: BorderRadius.circular(10.0),
             child: Image.network(
               product.urlToImage,
-              width: 115,
-              height: 115,
+              width: 405,
+              height: 200,
               fit: BoxFit.cover,
             ),
           ),
-          const SizedBox(width: 16.0),
+          const SizedBox(height: 15.0),
           ProductDetail(product: product)
         ],
       ),
