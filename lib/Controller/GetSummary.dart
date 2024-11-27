94f56580500ecef84d3dfaee0970b1dd0ce5d790
Jin Minu <s9430939@naver.com>
2024-11-27 18:05:42 +0900

    GetSummary.dart에 주석추가

--- lib/Controller/GetSummary.dart
+++ lib/Controller/GetSummary.dart
@@ -1,5 +1,11 @@
 import 'package:http/http.dart' as http;
 
+/*
+* 이 함수는 요약본을 제공하는 함수임
+* API를 호출하며 body에 link를 실어서 보낸다.
+* 요청된 python 함수에서는 전달받은 link에서 원문을 추출하고
+* 요약본을 response로 제공해준다.
+* */
 Future<String> getSummary(String newsLink) async {
   String? link = newsLink;
 
