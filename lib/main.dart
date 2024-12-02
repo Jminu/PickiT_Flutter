edf5568a1391861df763d7aa7745bc977dc82186
Jin Minu <s9430939@naver.com>
2024-12-02 12:31:17 +0900

    main.dart 주석제거

--- lib/main.dart
+++ lib/main.dart
@@ -35,17 +35,6 @@ void main() async {
   //showDB();
 
   runApp(const MyApp());
-
-
-  /**
-   * 이 부분이 실행되면, 뉴스기사 긁어옵니다.
-   * 모두가 사용하면 사용량이 많아져서 요금을 내야합니다. 따라서 제가 깃헙에 올릴때는 이부분 주석처리하고
-   * 올리겠습니다.
-   */
-  //앱 실행 후 2초마다 fetchUserId 호출
-  // Timer.periodic(Duration(seconds: 10), (timer) {
-  //   getFilteredFeeds();
-  // });
 }
 
 class MyApp extends StatelessWidget {
